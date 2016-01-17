a1c893c4d59829a2a3fb7748b99d99cb6d2b4460
Alex van Hoek - voorraad <alexandervanhoek@gmail.com>
2016-01-17 21:26:12 +0100

    New Model for cities

--- lk/models.py
+++ lk/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from todos.models import TimeStampedModel
 
-# Create your models here.
+class City(TimeStampedModel):
+    country = models.CharField(blank=True, null=True, max_length=255)
+    city_name = models.CharField(blank=True, null=True, max_length=255)
+    connected_cities = models.CharField(blank=True, null=True, max_length=4096)
+
+
+
